e94589f8ceb753b4331a6dffdebede1374349948
xavierrice <xavier.rice@gmail.com>
2023-07-07 14:49:31 -0400

    added a color border and the difficulty rating to the card

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ fetch("https://opentdb.com/api.php?amount=10")
         const results = JSONresponse.results
         for (let i = 0; i < results.length; i++) {
             displayCard(results[i])
+            test(results[i])
         }
     }).catch((err) => test(err))
 
@@ -17,12 +18,21 @@ function displayCard(questionObject) {
     const cards = document.createElement("article")
     cards.classList.add("card")
    
+    if(questionObject.difficulty === "medium"){
+        cards.style.border = "3px solid #e4e005";
+    }
+
+    if(questionObject.difficulty === "hard"){
+        cards.style.border = "3px solid #F01313"
+    }
+    const h4Rating = document.createElement("h4")
+    h4Rating.innerText = questionObject.difficulty
+
     const h2 = document.createElement("h2");
     h2.innerHTML = questionObject.category;
 
     const pQuestion = document.createElement("p")
     pQuestion.innerHTML = questionObject.question
-    test(questionObject.question)
 
     const pAnswer = document.createElement("p")
     pAnswer.classList.add("hidden")
@@ -35,7 +45,7 @@ function displayCard(questionObject) {
         pAnswer.style.display = "block"
     })
 
-    cards.append(h2, pQuestion, answerButton, pAnswer);
+    cards.append(h4Rating,h2, pQuestion, answerButton, pAnswer);
     main.append(cards);
     
 }
