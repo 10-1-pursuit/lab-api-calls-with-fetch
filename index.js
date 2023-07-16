13d44a2f24216cd3617518d3fb594ed61681bf4f
keniaDE <decoteaukenia@gmail.com>
2023-07-16 11:35:48 -0400

    lab

--- index.js
+++ index.js
@@ -4,14 +4,20 @@ fetch('https://opentdb.com/api.php?amount=10')
 // .then((JsonResponse) => console.log(JsonResponse.results) )
 
 function renderCards(arrayOfQuestionObjs) {
-    console.log(arrayOfQuestionObjs.question)
+    console.log(arrayOfQuestionObjs)
 
-}
+
 
 const cards = document.querySelector('main')
 const articleCard = document.createElement('article')
 const h2Card = document.createElement('h2')
+h2Card.innerText = 'Entertainment'
 articleCard.append(h2Card)
+const questionz = document.createElement('p')
+questionz.innerText = arrayOfQuestionObjs[1].question
+articleCard.append(questionz)
 const buttonCard = document.createElement('button')
+buttonCard.innerText= 'Short Answer '
 articleCard.append(buttonCard)
-
+cards.append(articleCard)
+}
